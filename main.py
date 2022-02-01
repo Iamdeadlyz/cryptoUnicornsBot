a6fad6d70d16b3cb57f4a84e8c096ab378788eeb
iamdeadlyz <29373963+Iamdeadlyz@users.noreply.github.com>
2022-02-01 22:28:50 +0800

    Add new commands - lootbox and UNIM
    Minted lootbox and UNIM tokens left in the darkforest contract can now be checked

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
-from cryptoUnicorn import remainingShadowCornEggs, getShadowCorns
+from cryptoUnicorn import remainingShadowCornEggs, getShadowCorns, totalLootBox, getLootBox, unimLeft, getUNIM
 
 ###########################################################################
 
@@ -37,7 +37,40 @@ async def shadowCorn(ctx:SlashContext):
   embed.add_field(name="\u200b", value="\u200b", inline=False)
   embed.add_field(name="Common", value="{} out of 2000 [-OS link-](https://opensea.io/assets/matic/0x99a558bdbde247c2b2716f0d4cfb0e246dfb697d/1)".format(remainingShadowCornEggs[0][0][0]), inline=False)
   embed.add_field(name="Rare", value="{} out of 950 [-OS link-](https://opensea.io/assets/matic/0x99a558bdbde247c2b2716f0d4cfb0e246dfb697d/2)".format(remainingShadowCornEggs[0][0][1]), inline=False)
-  embed.add_field(name="Epic", value="{} out of 50 [-OS link-](https://opensea.io/assets/matic/0x99a558bdbde247c2b2716f0d4cfb0e246dfb697d/3)".format(remainingShadowCornEggs[0][0][2]), inline=False)
+  embed.add_field(name="Mythic", value="{} out of 50 [-OS link-](https://opensea.io/assets/matic/0x99a558bdbde247c2b2716f0d4cfb0e246dfb697d/3)".format(remainingShadowCornEggs[0][0][2]), inline=False)
+  await ctx.send(embed=embed)
+
+###########################################################################
+
+@slash.slash(
+  name="lootbox",
+  description="Get the total minted lootbox",
+  guild_ids=[000000000000000], #replace the guildID here to your server ID
+  default_permission=True
+)
+
+async def lootbox(ctx:SlashContext):
+  getLootBox()
+  embed=discord.Embed(title="Crypto Unicorns Darkforest - Minted lootbox", description="\u200b", color=0xff00c8)
+  embed.set_thumbnail(url="https://lh3.googleusercontent.com/EorjfSP6_15XdWcMLzJqHTC-y1aadmGeL_jydEtpL4aTj--lUdQ1GWy_bdvb8PN6Dmf46rXOZxgYQkbDVEoNnjHrWKO9HUdc39RdPkA=s0")
+  embed.add_field(name="Rare Lootbox", value="{} minted. [-OS link-](https://opensea.io/assets/matic/0x99a558bdbde247c2b2716f0d4cfb0e246dfb697d/5)".format(totalLootBox["rare"]), inline=False)
+  embed.add_field(name="Mythic Lootbox", value="{} minted. [-OS link-](https://opensea.io/assets/matic/0x99a558bdbde247c2b2716f0d4cfb0e246dfb697d/6)".format(totalLootBox["mythic"]), inline=False)
+  await ctx.send(embed=embed)
+
+###########################################################################
+
+@slash.slash(
+  name="unimLeft",
+  description="Get the remaining UNIM tokens in the darkforest",
+  guild_ids=[000000000000000], #replace the guildID here to your server ID
+  default_permission=True
+)
+
+async def unim(ctx:SlashContext):
+  getUNIM()
+  embed=discord.Embed(title="Crypto Unicorns Darkforest - Remaining UNIM", description="\u200b", color=0xff00c8)
+  embed.set_thumbnail(url="https://lh3.googleusercontent.com/EorjfSP6_15XdWcMLzJqHTC-y1aadmGeL_jydEtpL4aTj--lUdQ1GWy_bdvb8PN6Dmf46rXOZxgYQkbDVEoNnjHrWKO9HUdc39RdPkA=s0")
+  embed.add_field(name="UNIM", value="{}".format(unimLeft["unim"]), inline=False)
   await ctx.send(embed=embed)
 
 ###########################################################################
